8a714137b93bd4312da70caa38855d5b2c1fefd6
FU XUE PAN <FU XUE PAN@192.168.253.1>
2017-07-26 11:29:53 +0800

    update

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -5,7 +5,9 @@ public class Test {
 	public static void main(String[] args) {
 		int i = 1;
 		System.out.println(i);
-
+		String s = "你猜猜";
+		int a = s.indexOf("你猜");
+		System.out.println(a);
 	}
 
 }
